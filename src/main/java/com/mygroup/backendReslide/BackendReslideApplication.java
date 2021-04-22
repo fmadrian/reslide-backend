0a4ff2acdebe680626d7cb5d7235a0300c1725a1
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-04-22 11:48:11 -0600

    Initial commit

---
+++ src/main/java/com/mygroup/backendReslide/BackendReslideApplication.java
@@ -0,0 +1,13 @@
+package com.mygroup.backendReslide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendReslideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendReslideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mygroup/backendReslide/BackendReslideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygroup.backendReslide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendReslideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

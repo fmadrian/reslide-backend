c92500a1b2de6b6e4e9ce9aa5155300999df337a
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-06-03 18:19:59 -0600

    Error handling system

---
+++ src/main/java/com/mygroup/backendReslide/dto/response/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.mygroup.backendReslide.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorResponse {
+    private String type;
+    private String message;
+}

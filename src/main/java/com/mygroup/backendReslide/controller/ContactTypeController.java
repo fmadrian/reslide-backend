574cb1ae4d42d4cbe22a3ebf87830f16e70c6da3
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-06-11 21:37:33 -0600

    Typo

--- src/main/java/com/mygroup/backendReslide/controller/ContactTypeController.java
+++ src/main/java/com/mygroup/backendReslide/controller/ContactTypeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/contact/type")
 @AllArgsConstructor
 public class ContactTypeController {
 
--- src/main/java/com/mygroup/backendReslide/controller/PaymentMethodController.java
+++ src/main/java/com/mygroup/backendReslide/controller/PaymentMethodController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/payment/method")
+@RequestMapping("/api/payment/method")
 @AllArgsConstructor
 public class PaymentMethodController {
     private final PaymentMethodService paymentMethodService;

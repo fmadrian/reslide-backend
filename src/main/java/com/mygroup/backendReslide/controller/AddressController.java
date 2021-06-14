3a1ff0dc1188977b10d5756d213b2405366abcd4
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-06-14 13:59:12 -0600

    Typo in address controller

--- src/main/java/com/mygroup/backendReslide/controller/AddressController.java
+++ src/main/java/com/mygroup/backendReslide/controller/AddressController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/contact")
+@RequestMapping("/api/address")
 @AllArgsConstructor
 public class AddressController {
     private final AddressService addressService;

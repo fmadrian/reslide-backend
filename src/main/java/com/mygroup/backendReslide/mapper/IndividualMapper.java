166d231e15b20693245d6e47f07bd6372258377e
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-07-15 14:05:31 -0600

    IndividualMapper string issue fixed
    Fixes issue where every string in individual mapper would get applied a function designed to convert a sentence to upper case and remove some characters.

--- src/main/java/com/mygroup/backendReslide/mapper/IndividualMapper.java
+++ src/main/java/com/mygroup/backendReslide/mapper/IndividualMapper.java
@@ -13,7 +13,10 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
@@ -25,12 +28,19 @@ public abstract class IndividualMapper {
     @Autowired
     private IndividualTypeRepository individualTypeRepository;
 
+    @Mapping(target = "code", expression = "java(prepareCode(individualRequest.getCode()))")
+    @Mapping(target = "name", expression = "java(individualRequest.getName())")
+    @Mapping(target = "notes", expression = "java(individualRequest.getNotes())")
     @Mapping(target = "enabled", expression = "java(true)")
     @Mapping(target = "addresses", expression = "java(mapAddressesToEntity(individualRequest.getAddresses()))")
     @Mapping(target = "contacts", expression = "java(mapContactsToEntity(individualRequest.getContacts()))")
     @Mapping(target = "type", expression = "java(getIndividualType(individualRequest.getType()))")
     public abstract Individual mapToEntity(IndividualDto individualRequest);
 
+
+    @Mapping(target = "name", expression = "java(individual.getName())")
+    @Mapping(target = "notes", expression = "java(individual.getNotes())")
+    @Mapping(target = "code", expression = "java(individual.getCode())")
     @Mapping(target = "type", expression = "java(individual.getType().getName())")
     @Mapping(target = "addresses", expression = "java(mapAddressesToDto(individual.getAddresses()))")
     @Mapping(target = "contacts", expression = "java(mapContactsToDto(individual.getContacts()))")
@@ -74,5 +84,21 @@ public abstract class IndividualMapper {
     IndividualType getIndividualType(String name){
         return individualTypeRepository.findByNameIgnoreCase(name)
                 .orElseThrow(()-> new IndividualTypeNotFoundException(name));
+    }
+    // TODO: The implementation calls this method for every string mapped.
+    // Takes the code to remove spaces and convert it to upper case
+    String prepareCode(String code){
+        String result;
+        List<String> bannedCharacters = Arrays.asList("-", "/", " ");
+        // Removes spaces and convert it to upper case.
+        result = code.trim()
+                .toUpperCase(Locale.ROOT);
+        // Remove banned characters
+        for (String character : bannedCharacters){
+            if(result.contains(character)) {
+                result = result.replace(character, "");
+            }
+        }
+        return result;
     }
 }

99bcaf74e399cc89d4d9c5e65a1636add811a95f
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2021-11-23 16:17:57 -0600

    Update MeasurementTypeController.java
    Typo

--- src/main/java/com/mygroup/backendReslide/controller/MeasurementTypeController.java
+++ src/main/java/com/mygroup/backendReslide/controller/MeasurementTypeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/measurement/type/")
+@RequestMapping("/api/measurement-type/")
 @AllArgsConstructor
 public class MeasurementTypeController {
 

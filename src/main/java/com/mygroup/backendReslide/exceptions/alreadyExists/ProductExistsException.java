7ce6dc7d8bd468e21ff14dd1d0601a912de51085
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2022-01-17 13:05:30 -0600

    Update ProductExistsException.java
    Typo

--- src/main/java/com/mygroup/backendReslide/exceptions/alreadyExists/ProductExistsException.java
+++ src/main/java/com/mygroup/backendReslide/exceptions/alreadyExists/ProductExistsException.java
@@ -2,6 +2,6 @@ package com.mygroup.backendReslide.exceptions.alreadyExists;
 
 public class ProductExistsException extends RuntimeException {
     public ProductExistsException(String code) {
-        super("Product brand " + code + " already exists.");
+        super("Product " + code + " already exists.");
     }
 }
